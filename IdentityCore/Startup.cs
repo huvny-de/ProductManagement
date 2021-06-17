8a1b1cbb145458846f181551c46eb2cb14cfd15c
Huy Vo <neo.pinhuy00@gmail.com>
2021-06-17 09:30:52 +0700

    edit appconfig/start service

--- IdentityCore/Startup.cs
+++ IdentityCore/Startup.cs
@@ -1,6 +1,8 @@
+using IdentityCore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,7 @@ namespace IdentityCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-           // services.AddDbContext<DbProductManagement>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
+            services.AddDbContext<IdentityCoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
